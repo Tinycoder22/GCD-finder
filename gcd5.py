66c2dcfc7de82713ae8c6ae541764d00881ecbd6
Reva Bhagwat <101451498+Tinycoder22@users.noreply.github.com>
2023-03-09 01:40:38 +0530

    efficiency using algorithm
    python programs are often more efficient due to algorithms. here we make use of a theorotical algorithm called the Euclid's algorithm. We assume that gcd(m,n)= gcd(n,m-n)

---
+++ gcd5.py
@@ -0,0 +1,11 @@
+#improved gcd making use of an algorithm to do the same.
+#Author: Tinycoder
+def gcd(m,n):
+    if m<n:
+        (m,n) = (n,m)
+    if (m%n)==0:
+        return(n)
+    else:
+        diff = m-n
+    return(gcd(n,diff))
+print(gcd(64,56))
