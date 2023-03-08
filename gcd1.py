2391695a9c92a7964e4b36a1e96b65931360b22b
Reva Bhagwat <101451498+Tinycoder22@users.noreply.github.com>
2023-03-09 00:21:43 +0530

    Add files via upload
    beginner level program to understand basic implementation. This program will be upgraded using different approaches to learn more about them in the future and solve other problems with the same approaches.

---
+++ gcd1.py
@@ -0,0 +1,21 @@
+#greatest common divisor 1
+#Author: Tinycoder
+def gcd(m,n):
+    fm = []
+    for i in range(1, m+1):
+        if m%i==0:
+            fm.append(i)
+    print(fm)
+
+    fn =[]
+    for j in range(1,n+1):
+        if n%j==0:
+            fn.append(j)
+    print(fn)
+
+    cf = []
+    for f in fm:
+        if f in fn:
+            cf.append(f)
+    print(cf[-1])
+gcd(14,63)
