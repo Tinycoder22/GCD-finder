e6351c051f5fb603b312c945eb2c76444cc0fcf9
Reva Bhagwat <101451498+Tinycoder22@users.noreply.github.com>
2023-03-09 01:12:21 +0530

    scanning with decrement

---
+++ gcd4.py
@@ -0,0 +1,11 @@
+#improved code for gcd using no lists but more accurate
+#Author: Tinycoder
+
+def gcd(m,n):
+    i = min(m, n)
+    while (i>0):
+        if m%i==0 and n%i==0:
+            return(i)
+        else:
+            i = i - 1
+print(gcd(14,63))
